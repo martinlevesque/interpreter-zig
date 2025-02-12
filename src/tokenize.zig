286ef7747c6d3ff9be929cacd093086b00557fb7
Martin Levesque <levesque.martin@gmail.com>
2025-02-11 20:16:16 -0500

    comment

--- src/tokenize.zig
+++ src/tokenize.zig
@@ -10,9 +10,16 @@ const Token = struct {
     err: ?TokenizeError = null,
 };
 
-const LexemeSetup = struct { currentIdentifier: []const u8, nextIdentifier: []const u8, thenIdentifier: []const u8, thenLexeme: []const u8 };
+const LexemeSetup = struct {
+    currentIdentifier: []const u8,
+    nextIdentifier: []const u8,
+    thenIdentifier: []const u8,
+    thenLexeme: []const u8,
+    skipUpTo: u8,
+    includeToken: bool = true,
+};
 
-fn tokenAt(input: []const u8, index: usize, line: *u32) !?Token {
+fn tokenAt(input: []const u8, index: usize, line: u32) !?Token {
     if (index >= input.len) {
         return null;
     }
@@ -21,52 +28,53 @@ fn tokenAt(input: []const u8, index: usize, line: *u32) !?Token {
 
     switch (char) {
         '(' => {
-            return Token{ .identifier = "LEFT_PAREN", .lexeme = "(", .lineNumber = line.* };
+            return Token{ .identifier = "LEFT_PAREN", .lexeme = "(", .lineNumber = line };
         },
         ')' => {
-            return Token{ .identifier = "RIGHT_PAREN", .lexeme = ")", .lineNumber = line.* };
+            return Token{ .identifier = "RIGHT_PAREN", .lexeme = ")", .lineNumber = line };
         },
         '{' => {
-            return Token{ .identifier = "LEFT_BRACE", .lexeme = "{", .lineNumber = line.* };
+            return Token{ .identifier = "LEFT_BRACE", .lexeme = "{", .lineNumber = line };
         },
         '}' => {
-            return Token{ .identifier = "RIGHT_BRACE", .lexeme = "}", .lineNumber = line.* };
+            return Token{ .identifier = "RIGHT_BRACE", .lexeme = "}", .lineNumber = line };
         },
         '<' => {
-            return Token{ .identifier = "LESS", .lexeme = "<", .lineNumber = line.* };
+            return Token{ .identifier = "LESS", .lexeme = "<", .lineNumber = line };
         },
         '>' => {
-            return Token{ .identifier = "GREATER", .lexeme = ">", .lineNumber = line.* };
+            return Token{ .identifier = "GREATER", .lexeme = ">", .lineNumber = line };
+        },
+        '/' => {
+            return Token{ .identifier = "SLASH", .lexeme = "/", .lineNumber = line };
         },
         ',' => {
-            return Token{ .identifier = "COMMA", .lexeme = ",", .lineNumber = line.* };
+            return Token{ .identifier = "COMMA", .lexeme = ",", .lineNumber = line };
         },
         '.' => {
-            return Token{ .identifier = "DOT", .lexeme = ".", .lineNumber = line.* };
+            return Token{ .identifier = "DOT", .lexeme = ".", .lineNumber = line };
         },
         '-' => {
-            return Token{ .identifier = "MINUS", .lexeme = "-", .lineNumber = line.* };
+            return Token{ .identifier = "MINUS", .lexeme = "-", .lineNumber = line };
         },
         '+' => {
-            return Token{ .identifier = "PLUS", .lexeme = "+", .lineNumber = line.* };
+            return Token{ .identifier = "PLUS", .lexeme = "+", .lineNumber = line };
         },
         ';' => {
-            return Token{ .identifier = "SEMICOLON", .lexeme = ";", .lineNumber = line.* };
+            return Token{ .identifier = "SEMICOLON", .lexeme = ";", .lineNumber = line };
         },
         '*' => {
-            return Token{ .identifier = "STAR", .lexeme = "*", .lineNumber = line.* };
+            return Token{ .identifier = "STAR", .lexeme = "*", .lineNumber = line };
         },
         '=' => {
-            return Token{ .identifier = "EQUAL", .lexeme = "=", .lineNumber = line.* };
+            return Token{ .identifier = "EQUAL", .lexeme = "=", .lineNumber = line };
         },
         '!' => {
-            return Token{ .identifier = "BANG", .lexeme = "!", .lineNumber = line.* };
+            return Token{ .identifier = "BANG", .lexeme = "!", .lineNumber = line };
         },
-        '\n' => {
-            line.* = line.* + 1;
-        },
+        '\n' => {},
         else => {
-            return Token{ .identifier = "", .lexeme = "", .inputChar = char, .lineNumber = line.*, .err = TokenizeError.LexicalError };
+            return Token{ .identifier = "", .lexeme = "", .inputChar = char, .lineNumber = line, .err = TokenizeError.LexicalError };
         },
     }
 
@@ -76,10 +84,42 @@ fn tokenAt(input: []const u8, index: usize, line: *u32) !?Token {
 fn setupMultiTokensLexemes() !std.ArrayList(LexemeSetup) {
     var lexemes = std.ArrayList(LexemeSetup).init(std.heap.page_allocator);
 
-    try lexemes.append(LexemeSetup{ .currentIdentifier = "EQUAL", .nextIdentifier = "EQUAL", .thenIdentifier = "EQUAL_EQUAL", .thenLexeme = "==" });
-    try lexemes.append(LexemeSetup{ .currentIdentifier = "BANG", .nextIdentifier = "EQUAL", .thenIdentifier = "BANG_EQUAL", .thenLexeme = "!=" });
-    try lexemes.append(LexemeSetup{ .currentIdentifier = "LESS", .nextIdentifier = "EQUAL", .thenIdentifier = "LESS_EQUAL", .thenLexeme = "<=" });
-    try lexemes.append(LexemeSetup{ .currentIdentifier = "GREATER", .nextIdentifier = "EQUAL", .thenIdentifier = "GREATER_EQUAL", .thenLexeme = ">=" });
+    try lexemes.append(LexemeSetup{
+        .currentIdentifier = "EQUAL",
+        .nextIdentifier = "EQUAL",
+        .thenIdentifier = "EQUAL_EQUAL",
+        .thenLexeme = "==",
+        .skipUpTo = '=',
+    });
+    try lexemes.append(LexemeSetup{
+        .currentIdentifier = "BANG",
+        .nextIdentifier = "EQUAL",
+        .thenIdentifier = "BANG_EQUAL",
+        .thenLexeme = "!=",
+        .skipUpTo = '=',
+    });
+    try lexemes.append(LexemeSetup{
+        .currentIdentifier = "LESS",
+        .nextIdentifier = "EQUAL",
+        .thenIdentifier = "LESS_EQUAL",
+        .thenLexeme = "<=",
+        .skipUpTo = '=',
+    });
+    try lexemes.append(LexemeSetup{
+        .currentIdentifier = "GREATER",
+        .nextIdentifier = "EQUAL",
+        .thenIdentifier = "GREATER_EQUAL",
+        .thenLexeme = ">=",
+        .skipUpTo = '=',
+    });
+    try lexemes.append(LexemeSetup{
+        .currentIdentifier = "SLASH",
+        .nextIdentifier = "SLASH",
+        .thenIdentifier = "SLASH_SLASH",
+        .thenLexeme = "//",
+        .skipUpTo = '\n',
+        .includeToken = false,
+    });
 
     return lexemes;
 }
@@ -87,18 +127,26 @@ fn setupMultiTokensLexemes() !std.ArrayList(LexemeSetup) {
 pub fn tokenize(input: []const u8) !std.ArrayList(Token) {
     var tokens = std.ArrayList(Token).init(std.heap.page_allocator);
     var line: u32 = 1;
-    var skipNext = false;
+    var skipUpTo: u8 = 0;
     const setupLexemes: std.ArrayList(LexemeSetup) = try setupMultiTokensLexemes();
     defer setupLexemes.deinit();
 
-    for (input, 0..) |_, i| {
-        if (skipNext) {
-            skipNext = false;
+    for (input, 0..) |char, i| {
+        if (skipUpTo != 0 and skipUpTo != char) {
             continue;
         }
 
-        const currentToken = try tokenAt(input, i, &line);
-        const nextToken = try tokenAt(input, i + 1, &line);
+        if (char == '\n') {
+            line = line + 1;
+        }
+
+        if (char == skipUpTo) {
+            skipUpTo = 0;
+            continue;
+        }
+
+        const currentToken = try tokenAt(input, i, line);
+        const nextToken = try tokenAt(input, i + 1, line);
 
         if (currentToken) |token| {
             if (nextToken) |givenNextToken| {
@@ -106,16 +154,18 @@ pub fn tokenize(input: []const u8) !std.ArrayList(Token) {
 
                 for (setupLexemes.items) |lexeme| {
                     if (std.mem.eql(u8, givenNextToken.identifier, lexeme.nextIdentifier) and std.mem.eql(u8, token.identifier, lexeme.currentIdentifier)) {
-                        try tokens.append(Token{ .identifier = lexeme.thenIdentifier, .lexeme = lexeme.thenLexeme, .lineNumber = line });
+                        if (lexeme.includeToken) {
+                            try tokens.append(Token{ .identifier = lexeme.thenIdentifier, .lexeme = lexeme.thenLexeme, .lineNumber = line });
+                        }
 
                         foundSetupLexeme = true;
+                        skipUpTo = lexeme.skipUpTo;
                         break;
                     }
                 }
 
                 if (foundSetupLexeme) {
                     // we found a multi tokens lexeme valid
-                    skipNext = true;
                     continue;
                 }
             }
