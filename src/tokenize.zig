d2cf26e6b80ad9f8e16c2c2e1136788d337e56d1
Martin Levesque <levesque.martin@gmail.com>
2025-02-16 20:01:42 -0500

    wip

--- src/tokenize.zig
+++ src/tokenize.zig
@@ -33,6 +33,7 @@ pub const TokenType = enum {
 const Token = struct {
     type: TokenType,
     lexeme: []const u8,
+    literalStr: []const u8 = "",
     inputChar: u8 = 0,
     lineNumber: u32 = 0,
     err: ?TokenizeError = null,
@@ -46,6 +47,7 @@ const LexemeSetup = struct {
     skipUpTo: u8 = 0,
     includeToken: bool = true,
     skipOnCurrent: bool = false,
+    advance: *const fn ([]const u8, usize) ?struct { usize, ?Token },
 };
 
 fn tokenAt(input: []const u8, index: usize, line: u32) !?Token {
@@ -116,6 +118,56 @@ fn tokenAt(input: []const u8, index: usize, line: u32) !?Token {
     return null;
 }
 
+fn charAtIndex(input: []const u8, i: usize, char: u8) ?usize {
+    if (i < input.len and input[i] == char) {
+        return i;
+    }
+
+    return null;
+}
+
+fn structCharAt(char: u8) type {
+    return struct {
+        fn f(input: []const u8, i: usize) ?usize {
+            return charAtIndex(input, i + 1, char);
+        }
+        const ptr = f; // Assign function pointer
+    };
+}
+
+fn structAdvanceFindNext(token: Token, char: u8) type {
+    return struct {
+        fn f(line: usize, input: []const u8, i: usize) ?Token {
+            for (i..input.len) |idx| {
+                if (input[idx] == char) {
+                    return Token{
+                        .type = token.type,
+                        .lexeme = token.lexeme,
+                        .inputChar = token.inputChar,
+                        .lineNumber = line,
+                    };
+                }
+            }
+            return null;
+        }
+        const ptr = f;
+    };
+}
+
+fn structAdvanceOne(token: Token, char: u8) type {
+    return struct {
+        fn f(line: usize, _: []const u8, i: usize) ?Token { // todo ret pair
+            return Token{
+                .type = token.type,
+                .lexeme = token.lexeme,
+                .inputChar = token.inputChar,
+                .lineNumber = line,
+            };
+        }
+        const ptr = f;
+    };
+}
+
 fn setupMultiTokensLexemes() !std.ArrayList(LexemeSetup) {
     var lexemes = std.ArrayList(LexemeSetup).init(std.heap.page_allocator);
 
@@ -125,35 +177,36 @@ fn setupMultiTokensLexemes() !std.ArrayList(LexemeSetup) {
         .thenType = TokenType.EQUAL_EQUAL,
         .thenLexeme = "==",
         .skipUpTo = '=',
+        .advance = structAdvanceFindNext(Token{ .type = TokenType.EQUAL_EQUAL, .inputChar = '=', .lexeme = "==" }, '=').ptr,
     });
     try lexemes.append(LexemeSetup{
         .currentType = TokenType.BANG,
         .nextType = TokenType.EQUAL,
         .thenType = TokenType.BANG_EQUAL,
         .thenLexeme = "!=",
-        .skipUpTo = '=',
+        .advance = structAdvanceFindNext(Token{ .type = TokenType.BANG_EQUAL, .inputChar = '!', .lexeme = "!=" }, '=').ptr,
     });
     try lexemes.append(LexemeSetup{
         .currentType = TokenType.LESS,
         .nextType = TokenType.EQUAL,
         .thenType = TokenType.LESS_EQUAL,
         .thenLexeme = "<=",
-        .skipUpTo = '=',
+        .advance = structAdvanceFindNext(Token{ .type = TokenType.LESS_EQUAL, .inputChar = '<', .lexeme = "<=" }, '=').ptr,
     });
     try lexemes.append(LexemeSetup{
         .currentType = TokenType.GREATER,
         .nextType = TokenType.EQUAL,
         .thenType = TokenType.GREATER_EQUAL,
         .thenLexeme = ">=",
-        .skipUpTo = '=',
+        .advance = structAdvanceFindNext(Token{ .type = TokenType.GREATER_EQUAL, .inputChar = '>', .lexeme = ">=" }, '=').ptr,
     });
     try lexemes.append(LexemeSetup{
         .currentType = TokenType.SLASH,
         .nextType = TokenType.SLASH,
         .thenType = TokenType.SLASH_SLASH,
         .thenLexeme = "//",
-        .skipUpTo = '\n',
         .includeToken = false,
+        .advance = structAdvanceFindNext(Token{ .type = TokenType.SLASH_SLASH, .inputChar = '/', .lexeme = "//" }, '\n').ptr,
     });
     try lexemes.append(LexemeSetup{
         .currentType = TokenType.SPACE,
@@ -162,7 +215,9 @@ fn setupMultiTokensLexemes() !std.ArrayList(LexemeSetup) {
         .thenLexeme = "",
         .includeToken = false,
         .skipOnCurrent = true,
+        .advance = structAdvanceOne(Token{ .type = TokenType.NONE, .inputChar = ' ', .lexeme = "" }, ' ').ptr,
     });
+
     try lexemes.append(LexemeSetup{
         .currentType = TokenType.TAB,
         .nextType = TokenType.NONE,
@@ -181,19 +236,13 @@ pub fn tokenize(input: []const u8) !std.ArrayList(Token) {
     var skipUpTo: u8 = 0;
     const setupLexemes: std.ArrayList(LexemeSetup) = try setupMultiTokensLexemes();
     defer setupLexemes.deinit();
+    var i: usize = 0;
 
-    for (input, 0..) |char, i| {
-        if (skipUpTo != 0 and skipUpTo != char) {
-            continue;
-        }
+    while (i < input.len) {
+        const char = input[i];
 
         if (char == '\n') {
             line = line + 1;
-        }
-
-        if (char == skipUpTo) {
-            skipUpTo = 0;
-            continue;
         }
 
         const currentToken = try tokenAt(input, i, line);
@@ -227,6 +276,7 @@ pub fn tokenize(input: []const u8) !std.ArrayList(Token) {
 
                 if (foundSetupLexeme) {
                     // we found a multi tokens lexeme valid
+                    i = i + 1;
                     continue;
                 }
             }
@@ -235,6 +285,8 @@ pub fn tokenize(input: []const u8) !std.ArrayList(Token) {
                 try tokens.append(token);
             }
         }
+
+        i = i + 1;
     }
 
     try tokens.append(Token{ .type = TokenType.EOF, .lexeme = "EOF" });
